9fd6958281183c76d389cbd6e1e878383224c1be
Brian Cancel <brian.e.cancel@gmail.com>
2020-07-20 19:46:30 -0400

    Updates

---
+++ precache-manifest.db697e86e91e0a7b904df64c2c51fe2c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd850f7c3cd5d2af126bb907111dbb6",
+    "url": "/doggoPR/index.html"
+  },
+  {
+    "revision": "0ad75d51ca44d10aec06",
+    "url": "/doggoPR/static/css/main.20bc27b3.chunk.css"
+  },
+  {
+    "revision": "5aca37444c0fd55eba35",
+    "url": "/doggoPR/static/js/2.7f8e63a0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/doggoPR/static/js/2.7f8e63a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad75d51ca44d10aec06",
+    "url": "/doggoPR/static/js/main.12d9e76e.chunk.js"
+  },
+  {
+    "revision": "50a2a4d6365aed7c1a5b",
+    "url": "/doggoPR/static/js/runtime-main.5e87ac7c.js"
+  },
+  {
+    "revision": "4fdc3f9e31ba3aef5c00595f3b8296d7",
+    "url": "/doggoPR/static/media/coller1.4fdc3f9e.jpeg"
+  },
+  {
+    "revision": "40c14b4a10845b3e2ea79636e3aa74d9",
+    "url": "/doggoPR/static/media/doggoColler1.40c14b4a.jpg"
+  },
+  {
+    "revision": "26b7bc8dffb32f49aea1abb4b1e1d246",
+    "url": "/doggoPR/static/media/doggoColler2.26b7bc8d.jpg"
+  },
+  {
+    "revision": "4b43e7143ff9c6bc35a9098a2f62b892",
+    "url": "/doggoPR/static/media/doggoColler3.4b43e714.jpg"
+  },
+  {
+    "revision": "6981b0c78c94e03aa595870deeacde5f",
+    "url": "/doggoPR/static/media/doggoFlavia.6981b0c7.JPG"
+  },
+  {
+    "revision": "e2d67efd404d26b579a7201516c68293",
+    "url": "/doggoPR/static/media/doggoInsta1.e2d67efd.jpeg"
+  },
+  {
+    "revision": "fc662999b04a17f0a4fcf6d3e45e688b",
+    "url": "/doggoPR/static/media/doggoTila1.fc662999.JPG"
+  },
+  {
+    "revision": "c1daaa0ff4da920f035dffeb126afa8d",
+    "url": "/doggoPR/static/media/doggoTila2.c1daaa0f.JPG"
+  }
+]);
